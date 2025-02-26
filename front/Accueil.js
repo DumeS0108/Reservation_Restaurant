4f2b9e201482af988f79283e65f0c4e974b2cc2d
SachaD <dumesacha@gmail.com>
2025-02-26 10:27:01 +0100

    Encore

--- front/Accueil.js
+++ front/Accueil.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 /* ✨ Animation Header au Scroll */
-window.onscroll = function() {
+window.onscroll = function () {
     var header = document.querySelector("header");
     if (window.scrollY > 50) {
         header.style.background = "rgba(139, 0, 0, 1)";
     } else {
         header.style.background = "rgba(139, 0, 0, 0.9)";
     }
-=======
-/* ✨ Animation Header au Scroll */
-window.onscroll = function() {
-    var header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.style.background = "rgba(139, 0, 0, 1)";
-    } else {
-        header.style.background = "rgba(139, 0, 0, 0.9)";
-    }
->>>>>>> d3020113088e0673e5f7f88f62cd0a897ddeafee
-};
+}
